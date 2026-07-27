700cd9c276a861a6986f41257e1a21dd3902df59
Tanveer <48864420+crxssed7@users.noreply.github.com>
2026-07-27 10:27:29 +0100

    Change XPath to select table by data-x-id
    Updated XPath selector to prevent IndexError.

--- src/main.py
+++ src/main.py
@@ -44,7 +44,8 @@ def get_language_details() -> list:
     content = html.fromstring(res.content)
 
     languages = []
-    table = content.xpath('.//table[@id="Table"]')[0]
+    # TODO: Prevent IndexError being thrown. Maybe alert somehow?
+    table = content.xpath('.//table[@data-x-id="Table"]')[0]
     for row in table.cssselect('tr'):
         cells = row.cssselect('td')
         if len(cells) == 0:
